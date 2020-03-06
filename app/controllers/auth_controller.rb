2d7cf7c53b9e1a69b71373608561df6a979bdae4
Amritpal Singh <singhamritpal49@gmail.com>
2020-03-06 13:22:34 -0500

    added signup add login API ENDPOINTS

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,14 @@
+class AuthController < ApplicationController
+
+    def login
+        user = User.find_by(username: params[:username])
+        is_authenticated = user.authenticate(params[:password])
+        if is_authenticated
+            render json: user
+        else 
+            render json: {error: "Wrong username/password. Please try again!"}
+    end
+end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+      # username validations
+    validates_presence_of :username
+    validates_uniqueness_of :username, :case_sensitive => false
+    validates :username, :length => { :in => 6..20}
+
+
+  # password validations
     has_secure_password
+    validates :password, :length => { :minimum => 6 }
 end
