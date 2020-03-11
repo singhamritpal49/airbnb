999b7797c349a1ce55482e53cd27b3485487c7da
Zermina Ejaz <zerminaejaz@gmail.com>
2020-03-11 00:18:47 -0400

    Added in AWS S3, added s3 controller, changed puma so rails runs on port 4000, added a direct_post action in s3 controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 's3/direct_post'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
